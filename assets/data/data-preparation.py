f194be03ce06e666ee5b7a0fcfd7bd8a85dd984f
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2024-05-30 14:47:48 +0200

    revert changes to original rework data-preparation to store everything in lists before printing

--- assets/data/data-preparation.py
+++ assets/data/data-preparation.py
@@ -6,6 +6,13 @@ import json
 import re
 import pandas as pd
 
+# settings: e.g. if the level 2 tags should be displayed: (level 0 = category, level 1 = subcategory, level 2 = subsubcategory)
+display_level_2 = False # TODO: actually I would need the same system as for the categories (print the category only once ---> print the subcategory only once when there are multiple subsubcategories)
+
+# TODO: in order to get e.g. "Input Data" checked, I have to check for "Input Data" and "Input Data > Data Types" and "Input Data > Data Types > Text" etc.
+# OR I have to tag "Input Data" by hand if there is a subcategory that is tagged
+
+
 # Load the zotero_data
 with open('main_clean.json', 'r') as f:
     zotero_data = json.load(f)
@@ -18,7 +25,7 @@ with open('main_clean_20.json', 'r') as f:
 # with open('table2-combCite.csv', 'w') as f:
 #     f.write("Paper#Ref,Adaptive Systems,Evaluation of Systems and Algorithms,Model Steering and Active Learning,Replication,Report Generation,Understanding the User,Grammar,Graph,Model,Sequence,Classification Models,Pattern Analysis,Probabilistic Models/Prediction,Program Synthesis,Interactive Visual Analysis\n")
 #     for item in zotero_data:
-#         f.write(f"{item['title']}#{item['id']},0,1,1,0,0,1,1,0,0,2,2,3,3,0,0\n") # todo add the actual codings
+#         f.write(f"{item['title']}#{item['id']},0,1,1,0,0,1,1,0,0,2,2,3,3,0,0\n") #
 
 
 # THIS ONE worked
@@ -36,7 +43,7 @@ with open('main_clean_20.json', 'r') as f:
 #         if item['title'] == "\"Picture the scene⋯\" visually summarising social media events":
 #             item['title'] = "\'Picture the scene...\' visually summarising social media events"
 #
-#         f.write(f"{item['title']}#{item['id']};0;1;1;0;0;1;1;0;0;2;2;3;3;0;0\n") # todo add the actual codings
+#         f.write(f"{item['title']}#{item['id']};0;1;1;0;0;1;1;0;0;2;2;3;3;0;0\n") #
 
 # with open('table2-combCite.csv', 'w') as f:
 #     f.write("Paper#Ref;Adaptive Systems;Evaluation of Systems and Algorithms;Model Steering and Active Learning;Replication;Report Generation;Understanding the User;Grammar;Graph;Model;Sequence;Classification Models;Pattern Analysis;Probabilistic Models/Prediction;Program Synthesis;Interactive Visual Analysis\n")
@@ -47,21 +54,56 @@ with open('main_clean_20.json', 'r') as f:
 
 df = pd.read_excel('Document Profiles.xlsx', sheet_name='Sheet1', header=0)
 
+
+# create a function for going over the header data from Document Profiles.xlsx
+# get the header from the MAXQDA data with df.columns and iterate over it, skipping the first 3 columns
+def write_header_from_maxqda(f, for_table2=False):
+    printed_categories = []
+    for col in df.columns[3:]:
+        if display_level_2:
+            if for_table2:
+                f.write(f"{col};")
+            else:
+                f.write(f"{col};none;;\n")
+        else:
+            # pattern = re.compile(r'^(?!\.\.)(.*?) > (.*)$')  # there is always a category and a subcategory, but not always a subsubcategory
+            pattern = re.compile(r'^(?!\.\.)(.*?)(?: > (.*))?$')  # this allows for no subcategory (e.g. "Input Modalities")
+            # now use this pattern to extract the title from the current row
+            match = pattern.match(col)
+            if match:
+                # first: check if the category is already printed
+                category = match.group(1)
+                if category not in printed_categories:
+                    printed_categories.append(category)
+                    if for_table2:
+                        f.write(f"{category};")
+                    else:
+                        f.write(f"{category};none;;\n")
+                if match.group(2):
+                    if for_table2:
+                        f.write(f"{match.group(2)};")
+                    else:
+                        f.write(f"{match.group(2)};none;;\n")  # print the subcategory
+            else:
+                # do nothing
+                print(f"No match: {col}")
+    f.write("\n")
+
+
 # write into the header.csv file
 with open('header.csv', 'w') as f:
     f.write("col_name;img_src;class;category\n")
     # get the header from the MAXQDA data with df.columns and iterate over it, skipping the first 3 columns
-    for col in df.columns[3:]:
-        f.write(f"{col};none;;\n")
-    f.write("\n")
-
+    write_header_from_maxqda(f)
 
 with open('table2-combCite.csv', 'w') as f:
     f.write("Paper#Ref;")
     # get the header from the MAXQDA data with df.columns and iterate over it, skipping the first 3 columns
-    for col in df.columns[3:]:
-        f.write(f"{col};")
-    f.write("\n")
+    # for col in df.columns[3:]:
+    #     f.write(f"{col};")
+    # f.write("\n")
+
+    write_header_from_maxqda(f, True)
 
     # f.write("Paper#Ref;Adaptive Systems;Evaluation of Systems and Algorithms;Model Steering and Active Learning;Replication;Report Generation;Understanding the User;Grammar;Graph;Model;Sequence;Classification Models;Pattern Analysis;Probabilistic Models/Prediction;Program Synthesis;Interactive Visual Analysis\n")
     for item in zotero_data:
@@ -77,9 +119,8 @@ with open('table2-combCite.csv', 'w') as f:
         if item['title'] == "\"Picture the scene⋯\" visually summarising social media events":
             item['title'] = "\'Picture the scene...\' visually summarising social media events"
 
-        # f.write(f"{item['title']}#{item['id']};0;1;1;0;0;1;1;0;0;2;2;3;3;0;0\n")
-        # todo add the actual codings
-
+        # testcoding: f.write(f"{item['title']}#{item['id']};0;1;1;0;0;1;1;0;0;2;2;3;3;0;0\n")
+        # add the actual codings
         zotero_title = item['title']
         f.write(f"{zotero_title}#{item['id']};")
         # find the corresponding MAXQDA data
@@ -93,7 +134,7 @@ with open('table2-combCite.csv', 'w') as f:
             # now use this pattern to extract the title from the current row
             match = pattern.match(row['Document name'])
             if match:
-                maxqda_title = match.group(1) # group(1) matches the first part of the pattern that is enclosed in brackets ()
+                maxqda_title = match.group(1)  # group(1) matches the first part of the pattern that is enclosed in brackets ()
             else:
                 print(f"no match for {row['Document name']}")
                 maxqda_title = row['Document name']
@@ -107,13 +148,13 @@ with open('table2-combCite.csv', 'w') as f:
             if maxqda_title in zotero_title:
                 for col in df.columns[3:]:
                     # if the value is not 0, write the number according to class to the file
-                    pattern = re.compile(r'^(?!\.\.)(.*?) > (.*)$')# there is always a category and a subcategory, but not always a subsubcategory
+                    pattern = re.compile(r'^(?!\.\.)(.*?) > (.*)$')  # there is always a category and a subcategory, but not always a subsubcategory
                     # now use this pattern to extract the title from the current row
                     match = pattern.match(col)
                     if match:
                         # print(f"Match: {col}")
                         category_number = 1
-                        category = match.group(1) # the category. Depending on the category, the class will be different, e.g. 0, 1, 2, 3
+                        category = match.group(1)  # the category. Depending on the category, the class will be different, e.g. 0, 1, 2, 3
                         if category == "Input Modalities":
                             category_number = 2
                         elif category == "Derived Data":
@@ -134,17 +175,19 @@ with open('table2-combCite.csv', 'w') as f:
                             category_number = 10
                         elif category == "Evaluation":
                             category_number = 11
-                        else: # TODO: this should not happen
+                        else:  # TODO: this should not happen
                             print(f"Category not found: {category}")
                             category_number = 1
 
-
-                        subcategory = match.group(2) # the actual tag
-                        # the third level, subsubcategory is not needed here
+                        subcategory = match.group(2)  # the actual tag
+                        # the level 2 , subsubcategory is not needed here
                     else:
-                        # TODO: in the third level, the highest-level-category is not displayed anymore ==> will not be found...
+                        # TODO: in the level2 or lower, the highest-level-category is not displayed anymore ==> will not be found...
                         # e.g. "..> Supplementary Data >  Popularity" instead of "Input Modalities > Supplementary Data >  Popularity"
                         print(f"No match: {col}")
+                        if not display_level_2:
+                            # skip the printing of the subsubcategory
+                            continue
 
                     # for this row and this column, find which number is in the xlsx file
                     if row[col] != 0:
@@ -153,7 +196,6 @@ with open('table2-combCite.csv', 'w') as f:
                         f.write("0;")
 
 
+
                 break
         f.write("\n")
-
-
